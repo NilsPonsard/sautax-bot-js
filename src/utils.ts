377e5c58513eb23f09fd6a4c49b33f86913374cd
Nils <nils.ponsard@etu.univ-amu.fr>
2020-04-28 21:50:45 +0200

    lol changes

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,18 @@ import * as Discord from "discord.js"
 import { readFileSync } from "fs"
 import * as main from "./server"
 
+interface botTokens {
+    "discord": string,
+    "riot": string
+}
+
+
+let tokensFile = readFileSync("tokens.json")
+let tokensParsed = JSON.parse(tokensFile.toString())
+
+export let tokens: botTokens = tokensParsed
+
+
 export function parse(msg: Discord.Message): Array<string> {
     return msg.content.replace(main.prefix, "").replace("\n", "").split(" ")
 }
