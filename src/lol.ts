cab862bfbae18a57481ed164a606f80784ebbdf6
Nils Ponsard <nils.ponsard@etu.univ-amu.fr>
2020-04-29 22:26:32 +0100

    game type

--- src/lol.ts
+++ src/lol.ts
@@ -9,6 +9,973 @@ import { promises, LookupOneOptions } from "dns";
 import { parse } from "path";
 import { parentPort } from "worker_threads";
 
+
+let queues = [
+
+    {
+
+        "queueId": 0,
+
+        "map": "Custom games",
+
+        "description": null,
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 2,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Blind Pick games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 430"
+
+    },
+
+    {
+
+        "queueId": 4,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Solo games",
+
+        "notes": "Deprecated in favor of queueId 420"
+
+    },
+
+    {
+
+        "queueId": 6,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Premade games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 7,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs AI games",
+
+        "notes": "Deprecated in favor of queueId 32 and 33"
+
+    },
+
+    {
+
+        "queueId": 8,
+
+        "map": "Twisted Treeline",
+
+        "description": "3v3 Normal games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 460"
+
+    },
+
+    {
+
+        "queueId": 9,
+
+        "map": "Twisted Treeline",
+
+        "description": "3v3 Ranked Flex games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 470"
+
+    },
+
+    {
+
+        "queueId": 14,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Draft Pick games",
+
+        "notes": "Deprecated in favor of queueId 400"
+
+    },
+
+    {
+
+        "queueId": 16,
+
+        "map": "Crystal Scar",
+
+        "description": "5v5 Dominion Blind Pick games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 17,
+
+        "map": "Crystal Scar",
+
+        "description": "5v5 Dominion Draft Pick games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 25,
+
+        "map": "Crystal Scar",
+
+        "description": "Dominion Co-op vs AI games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 31,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs AI Intro Bot games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 830"
+
+    },
+
+    {
+
+        "queueId": 32,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs AI Beginner Bot games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 840"
+
+    },
+
+    {
+
+        "queueId": 33,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs AI Intermediate Bot games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 850"
+
+    },
+
+    {
+
+        "queueId": 41,
+
+        "map": "Twisted Treeline",
+
+        "description": "3v3 Ranked Team games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 42,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Team games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 52,
+
+        "map": "Twisted Treeline",
+
+        "description": "Co-op vs AI games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 800"
+
+    },
+
+    {
+
+        "queueId": 61,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Team Builder games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 65,
+
+        "map": "Howling Abyss",
+
+        "description": "5v5 ARAM games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 450"
+
+    },
+
+    {
+
+        "queueId": 67,
+
+        "map": "Howling Abyss",
+
+        "description": "ARAM Co-op vs AI games",
+
+        "notes": "Game mode deprecated"
+
+    },
+
+    {
+
+        "queueId": 70,
+
+        "map": "Summoner's Rift",
+
+        "description": "One for All games",
+
+        "notes": "Deprecated in patch 8.6 in favor of queueId 1020"
+
+    },
+
+    {
+
+        "queueId": 72,
+
+        "map": "Howling Abyss",
+
+        "description": "1v1 Snowdown Showdown games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 73,
+
+        "map": "Howling Abyss",
+
+        "description": "2v2 Snowdown Showdown games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 75,
+
+        "map": "Summoner's Rift",
+
+        "description": "6v6 Hexakill games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 76,
+
+        "map": "Summoner's Rift",
+
+        "description": "Ultra Rapid Fire games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 78,
+
+        "map": "Howling Abyss",
+
+        "description": "One For All: Mirror Mode games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 83,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs AI Ultra Rapid Fire games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 91,
+
+        "map": "Summoner's Rift",
+
+        "description": "Doom Bots Rank 1 games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 950"
+
+    },
+
+    {
+
+        "queueId": 92,
+
+        "map": "Summoner's Rift",
+
+        "description": "Doom Bots Rank 2 games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 950"
+
+    },
+
+    {
+
+        "queueId": 93,
+
+        "map": "Summoner's Rift",
+
+        "description": "Doom Bots Rank 5 games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 950"
+
+    },
+
+    {
+
+        "queueId": 96,
+
+        "map": "Crystal Scar",
+
+        "description": "Ascension games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 910"
+
+    },
+
+    {
+
+        "queueId": 98,
+
+        "map": "Twisted Treeline",
+
+        "description": "6v6 Hexakill games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 100,
+
+        "map": "Butcher's Bridge",
+
+        "description": "5v5 ARAM games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 300,
+
+        "map": "Howling Abyss",
+
+        "description": "Legend of the Poro King games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 920"
+
+    },
+
+    {
+
+        "queueId": 310,
+
+        "map": "Summoner's Rift",
+
+        "description": "Nemesis games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 313,
+
+        "map": "Summoner's Rift",
+
+        "description": "Black Market Brawlers games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 315,
+
+        "map": "Summoner's Rift",
+
+        "description": "Nexus Siege games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 940"
+
+    },
+
+    {
+
+        "queueId": 317,
+
+        "map": "Crystal Scar",
+
+        "description": "Definitely Not Dominion games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 318,
+
+        "map": "Summoner's Rift",
+
+        "description": "ARURF games",
+
+        "notes": "Deprecated in patch 7.19 in favor of queueId 900"
+
+    },
+
+    {
+
+        "queueId": 325,
+
+        "map": "Summoner's Rift",
+
+        "description": "All Random games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 400,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Draft Pick games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 410,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Dynamic games",
+
+        "notes": "Game mode deprecated in patch 6.22"
+
+    },
+
+    {
+
+        "queueId": 420,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Solo games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 430,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Blind Pick games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 440,
+
+        "map": "Summoner's Rift",
+
+        "description": "5v5 Ranked Flex games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 450,
+
+        "map": "Howling Abyss",
+
+        "description": "5v5 ARAM games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 460,
+
+        "map": "Twisted Treeline",
+
+        "description": "3v3 Blind Pick games",
+
+        "notes": "Deprecated in patch 9.23"
+
+    },
+
+    {
+
+        "queueId": 470,
+
+        "map": "Twisted Treeline",
+
+        "description": "3v3 Ranked Flex games",
+
+        "notes": "Deprecated in patch 9.23"
+
+    },
+
+    {
+
+        "queueId": 600,
+
+        "map": "Summoner's Rift",
+
+        "description": "Blood Hunt Assassin games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 610,
+
+        "map": "Cosmic Ruins",
+
+        "description": "Dark Star: Singularity games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 700,
+
+        "map": "Summoner's Rift",
+
+        "description": "Clash games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 800,
+
+        "map": "Twisted Treeline",
+
+        "description": "Co-op vs. AI Intermediate Bot games",
+
+        "notes": "Deprecated in patch 9.23"
+
+    },
+
+    {
+
+        "queueId": 810,
+
+        "map": "Twisted Treeline",
+
+        "description": "Co-op vs. AI Intro Bot games",
+
+        "notes": "Deprecated in patch 9.23"
+
+    },
+
+    {
+
+        "queueId": 820,
+
+        "map": "Twisted Treeline",
+
+        "description": "Co-op vs. AI Beginner Bot games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 830,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs. AI Intro Bot games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 840,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs. AI Beginner Bot games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 850,
+
+        "map": "Summoner's Rift",
+
+        "description": "Co-op vs. AI Intermediate Bot games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 900,
+
+        "map": "Summoner's Rift",
+
+        "description": "URF games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 910,
+
+        "map": "Crystal Scar",
+
+        "description": "Ascension games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 920,
+
+        "map": "Howling Abyss",
+
+        "description": "Legend of the Poro King games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 940,
+
+        "map": "Summoner's Rift",
+
+        "description": "Nexus Siege games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 950,
+
+        "map": "Summoner's Rift",
+
+        "description": "Doom Bots Voting games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 960,
+
+        "map": "Summoner's Rift",
+
+        "description": "Doom Bots Standard games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 980,
+
+        "map": "Valoran City Park",
+
+        "description": "Star Guardian Invasion: Normal games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 990,
+
+        "map": "Valoran City Park",
+
+        "description": "Star Guardian Invasion: Onslaught games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1000,
+
+        "map": "Overcharge",
+
+        "description": "PROJECT: Hunters games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1010,
+
+        "map": "Summoner's Rift",
+
+        "description": "Snow ARURF games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1020,
+
+        "map": "Summoner's Rift",
+
+        "description": "One for All games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1030,
+
+        "map": "Crash Site",
+
+        "description": "Odyssey Extraction: Intro games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1040,
+
+        "map": "Crash Site",
+
+        "description": "Odyssey Extraction: Cadet games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1050,
+
+        "map": "Crash Site",
+
+        "description": "Odyssey Extraction: Crewmember games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1060,
+
+        "map": "Crash Site",
+
+        "description": "Odyssey Extraction: Captain games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1070,
+
+        "map": "Crash Site",
+
+        "description": "Odyssey Extraction: Onslaught games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1090,
+
+        "map": "Convergence",
+
+        "description": "Teamfight Tactics games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1100,
+
+        "map": "Convergence",
+
+        "description": "Ranked Teamfight Tactics games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1110,
+
+        "map": "Convergence",
+
+        "description": "Teamfight Tactics Tutorial games",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 1200,
+
+        "map": "Nexus Blitz",
+
+        "description": "Nexus Blitz games",
+
+        "notes": "Deprecated in patch 9.2"
+
+    },
+
+    {
+
+        "queueId": 2000,
+
+        "map": "Summoner's Rift",
+
+        "description": "Tutorial 1",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 2010,
+
+        "map": "Summoner's Rift",
+
+        "description": "Tutorial 2",
+
+        "notes": null
+
+    },
+
+    {
+
+        "queueId": 2020,
+
+        "map": "Summoner's Rift",
+
+        "description": "Tutorial 3",
+
+        "notes": null
+
+    }
+
+]
+
+
+
 interface sumSpellImage {
     full: string;
     sprite: string;
@@ -17,6 +984,12 @@ interface sumSpellImage {
     y: number;
     w: number;
     h: number;
+}
+
+interface sumSpellJSON {
+    type: string,
+    version: string,
+    data: { [key: string]: sumSpell }
 }
 
 interface sumSpell {
@@ -391,7 +1364,7 @@ let items = {}
 let champions: championJSON
 let runes: Array<RuneCategory>
 let version = ""
-let sumSpells: Array<sumSpell>
+let sumSpells: sumSpellJSON
 
 let lolHttpsOptions = {
     "headers": {
@@ -439,14 +1412,14 @@ https.get('https://ddragon.leagueoflegends.com/api/versions.json', (res: http.In
                 runes = JSON.parse(data)
             })
         })
-        https.get(dddragonBaseURL + "/data/fr_FR/runesReforged.json", (res) => {
+        https.get(dddragonBaseURL + "/data/fr_FR/summoner.json", (res) => {
 
             let data = ""
             res.on("data", (chunck) => {
                 data = data + chunck
             })
             res.on("end", () => {
-                runes = JSON.parse(data)
+                sumSpells = JSON.parse(data)
             })
         })
     })
@@ -559,12 +1532,50 @@ function spectate(msg: Discord.Message) {
         spectator(value.id).then((result) => {
             let blueSide = ""
             let redSide = ""
+            let gameMode = ""
+            queues.forEach((value, index) => {
+                if (value.queueId == result.gameQueueConfigId) {
+                    if (value.description) {
+                        gameMode = value.description.replace("games", "game")
 
+                    }
+                }
+            })
             for (let i = 0; i < result.participants.length; ++i) {
                 let current = result.participants[i]
                 let championId = current.championId.toString()
                 let championName = ""
-                console.log(current.spell1Id)
+                let sumSpell1 = ""
+                let sumSpell2 = ""
+                let mainPerk = current.perks.perkIds[0]
+                let mainPerkName = ""
+                for (let j = 0; j < runes.length; ++j) {
+                    let currPage = runes[j]
+                    for (let a = 0; a < currPage.slots.length; ++a) {
+                        let currSlot = currPage.slots[a]
+                        for (let b = 0; b < currSlot.runes.length; ++b) {
+                            let currRune = currSlot.runes[b]
+                            if (currRune.id == mainPerk) {
+                                mainPerkName = currRune.name
+                            }
+
+                        }
+
+                    }
+                }
+
+
+                for (let p in sumSpells.data) {
+                    let curr = sumSpells.data[p]
+                    if (current.spell1Id.toString() == curr.key) {
+                        sumSpell1 = curr.name
+                    }
+                    if (current.spell2Id.toString() == curr.key) {
+                        sumSpell2 = curr.name
+                    }
+                }
+
+
                 for (let p in champions.data) {
                     let curr = champions.data[p]
 
@@ -573,7 +1584,7 @@ function spectate(msg: Discord.Message) {
                     }
                 }
 
-                let currentText = `${current.bot ? "BOT " : ""} **${current.summonerName}** : ${championName} \n`
+                let currentText = `${current.bot ? "BOT " : ""} **${current.summonerName}** : ${championName} *${mainPerkName}* \`${sumSpell1 + "|" + sumSpell2}\` \n`
                 if (current.teamId == 100) {
                     blueSide = blueSide + currentText
                 }
@@ -581,10 +1592,25 @@ function spectate(msg: Discord.Message) {
                     redSide = redSide + currentText
                 }
             }
+            let bans = ""
+            for (let i = 0; i < result.bannedChampions.length; ++i) {
+                let championId = result.bannedChampions[i].championId
+                for (let p in champions.data) {
+                    let curr = champions.data[p]
+
+                    if (curr.key == championId.toString()) {
+                        bans = bans + curr.name + "\n"
+                    }
+                }
+            }
+
+
+
             let embed = new Discord.RichEmbed()
-            embed.setTitle(`${summonerName}’s game`)
+            embed.setTitle(`${summonerName}’s ${gameMode}`)
             embed.addField("Blue team", blueSide)
             embed.addField("Red team", redSide)
+            embed.addField("Bans", bans)
             msg.channel.send("", embed)
         }, (reason) => {
             msg.channel.send(JSON.stringify(reason))
